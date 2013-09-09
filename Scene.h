d8000aed55fcbd4bc5b59b01d762176179b2feb1
Otzeloot <jung.oskar@gmail.com>
2013-09-09 15:06:12 +0200

    Make Mr. Brandenburg proud.

--- Scene.h
+++ Scene.h
@@ -2,16 +2,50 @@
 
 #include "ofMain.h"
 
+/**
+ * The Scene class is the skeleton of every actual scene in the ViMuc
+ * application. It provides the OF methods to be overwritten by the individual
+ * scenes. Furthermore the scene class provides it's children the FFT data
+ * in two arrays. One array has the plain fft data, the other has the smoothed
+ * fft values.
+ */
 class Scene : public ofBaseApp {
   public:
+    /**
+     * Initializes the scene with a default of 10 bands.
+     */
     Scene();
 
+    /**
+     * The setup method is meant to resemble the OF default setup method.
+     * The OF default method delegates to this setup method.
+     */
     void setup();
+
+    /**
+     * The update method is meant to resemble the OF default update method.
+     * The OF default method delegates to this update method.
+     */
     void update();
+
+    /**
+     * The draw method is meant to resemble the OF default draw method.
+     * The OF default method delegates to this draw method.
+     */
     void draw();
 
+    /**
+     * The draw method is meant to resemble the OF default draw method.
+     * The OF default method delegates to this draw method.
+     */
     void keyPressed(int key);
+
+    /**
+     * The draw method is meant to resemble the OF default draw method.
+     * The OF default method delegates to this draw method.
+     */
     void keyReleased(int key);
+
     void mouseMoved(int x, int y);
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
